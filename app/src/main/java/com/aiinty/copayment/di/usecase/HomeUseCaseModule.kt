07a94745c10ce3ab19182eae3e3316ee971c3011
aiinty <thisissparta2016@mail.ru>
2025-06-17 21:02:19 +0600

    feat: home use cases

---
+++ app/src/main/java/com/aiinty/copayment/di/usecase/HomeUseCaseModule.kt
@@ -0,0 +1,25 @@
+package com.aiinty.copayment.di.usecase
+
+import com.aiinty.copayment.domain.repository.CardRepository
+import com.aiinty.copayment.domain.repository.TransactionRepository
+import com.aiinty.copayment.domain.usecase.card.GetCardsUseCase
+import com.aiinty.copayment.domain.usecase.card.InsertCardUseCase
+import com.aiinty.copayment.domain.usecase.card.UpdateCardUseCase
+import com.aiinty.copayment.domain.usecase.home.GetTransactionsUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HomeUseCaseModule {
+
+    @Provides
+    fun provideGetTransactionsUseCase(
+        transactionRepository: TransactionRepository
+    ): GetTransactionsUseCase {
+        return GetTransactionsUseCase(transactionRepository)
+    }
+
+}
